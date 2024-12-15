f63206a0e0843e1e0bedbab266a55fd3e675e2ec
Amelia Dewi Agustiani <ameliadewi1929@gmail.com>
2024-12-15 14:32:32 +0700

    update env

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,11 +3,13 @@ const sequelize = require('../config/connection'); // Adjust if necessary
 
 // Import all models
 const User = require('./user')(sequelize, DataTypes);
+const Course = require('./course')(sequelize, DataTypes);
 
 // Add associations here if necessary
 User.associate({});
 
 module.exports = {
   sequelize,
-  User,  // Export User model
+  User,
+  Course,
 };
