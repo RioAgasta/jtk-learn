044196ab190ef9a18e3b5ca114e9c90bcfbe3401
RioAgasta <rioagasta@gmail.com>
2025-02-09 16:52:47 +0700

    changed name on dashboard

--- frontend/src/pages/DashboardPelajarPage.js
+++ frontend/src/pages/DashboardPelajarPage.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 const DashboardPelajar = () => {
+    let nama = localStorage.getItem('nama');
 
     return (
         <div className="container mt-4">
           <div className="d-flex justify-content-between align-items-center mb-4">
-            <h3 className="fw-bold mb-0">Halo Pelajar</h3>
+            <h3 className="fw-bold mb-0">Halo {nama}</h3>
             <h3 className="fw-bold mb-0">Courses</h3>
           </div>
         </div>
